c4b2aed8586f720cb2a9b07629a901000a8ecf3b
beste küçük <peribeste48@gmail.com>
2022-05-14 19:41:34 +0300

    feat: addition project classes

---
+++ training-exam-app/Concrete/Answers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace training_exam_app.Concrete
+{
+    internal class Answers
+    {
+        public int Id { get; set; } 
+        public bool AnswerState { get; set; }
+        public int QuestionID { get; set; } 
+        public string AnswerImagePath { get; set; 
+    }
+}

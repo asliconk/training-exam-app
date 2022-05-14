388aee1758f2054106965323d7aed6ceb138cb2a
Aslı Cönk <75905402+asliconk@users.noreply.github.com>
2022-05-14 17:12:11 +0300

    initial commit

---
+++ training-exam-app/training-exam-app/Form1.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.XtraBars;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace training_exam_app
+{
+    public partial class Form1 : DevExpress.XtraBars.FluentDesignSystem.FluentDesignForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}

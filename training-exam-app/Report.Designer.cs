13a4921400daf70eefd66a6b7948e82ab44f7719
beste küçük <peribeste48@gmail.com>
2022-05-23 01:00:11 +0300

    refactor:design report form

--- training-exam-app/Report.Designer.cs
+++ training-exam-app/Report.Designer.cs
@@ -29,74 +29,157 @@ namespace training_exam_app
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Report));
             this.lblFirstName = new DevExpress.XtraEditors.LabelControl();
             this.lblLastName = new DevExpress.XtraEditors.LabelControl();
             this.lblHeader = new DevExpress.XtraEditors.LabelControl();
             this.lblModuleName = new DevExpress.XtraEditors.LabelControl();
-            this.lblSuccessPercent = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.svgImageBox1 = new DevExpress.XtraEditors.SvgImageBox();
+            this.svgImageBox2 = new DevExpress.XtraEditors.SvgImageBox();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.progressBarControl1 = new DevExpress.XtraEditors.ProgressBarControl();
+            this.progressBarControl2 = new DevExpress.XtraEditors.ProgressBarControl();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl2.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // lblFirstName
             // 
             this.lblFirstName.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.lblFirstName.Appearance.Options.UseFont = true;
-            this.lblFirstName.Location = new System.Drawing.Point(44, 79);
+            this.lblFirstName.Location = new System.Drawing.Point(76, 133);
+            this.lblFirstName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lblFirstName.Name = "lblFirstName";
-            this.lblFirstName.Size = new System.Drawing.Size(143, 21);
+            this.lblFirstName.Size = new System.Drawing.Size(137, 17);
             this.lblFirstName.TabIndex = 40;
-            this.lblFirstName.Text = "Student First Name";
+            this.lblFirstName.Text = "Student First Name-->";
             // 
             // lblLastName
             // 
             this.lblLastName.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.lblLastName.Appearance.Options.UseFont = true;
-            this.lblLastName.Location = new System.Drawing.Point(44, 135);
+            this.lblLastName.Location = new System.Drawing.Point(76, 179);
+            this.lblLastName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lblLastName.Name = "lblLastName";
-            this.lblLastName.Size = new System.Drawing.Size(141, 21);
+            this.lblLastName.Size = new System.Drawing.Size(137, 17);
             this.lblLastName.TabIndex = 41;
-            this.lblLastName.Text = "Student Last Name";
+            this.lblLastName.Text = "Student Last Name-->";
             // 
             // lblHeader
             // 
             this.lblHeader.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.lblHeader.Appearance.Options.UseFont = true;
-            this.lblHeader.Location = new System.Drawing.Point(391, 24);
+            this.lblHeader.Location = new System.Drawing.Point(327, 52);
+            this.lblHeader.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lblHeader.Name = "lblHeader";
-            this.lblHeader.Size = new System.Drawing.Size(112, 21);
+            this.lblHeader.Size = new System.Drawing.Size(117, 17);
             this.lblHeader.TabIndex = 42;
-            this.lblHeader.Text = "Student Report";
+            this.lblHeader.Text = "STUDENT REPORT";
             // 
             // lblModuleName
             // 
             this.lblModuleName.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.lblModuleName.Appearance.Options.UseFont = true;
-            this.lblModuleName.Location = new System.Drawing.Point(142, 230);
+            this.lblModuleName.Location = new System.Drawing.Point(367, 130);
+            this.lblModuleName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lblModuleName.Name = "lblModuleName";
-            this.lblModuleName.Size = new System.Drawing.Size(101, 21);
+            this.lblModuleName.Size = new System.Drawing.Size(82, 17);
             this.lblModuleName.TabIndex = 43;
             this.lblModuleName.Text = "Module Name";
             // 
-            // lblSuccessPercent
+            // labelControl1
             // 
-            this.lblSuccessPercent.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblSuccessPercent.Appearance.Options.UseFont = true;
-            this.lblSuccessPercent.Location = new System.Drawing.Point(635, 230);
-            this.lblSuccessPercent.Name = "lblSuccessPercent";
-            this.lblSuccessPercent.Size = new System.Drawing.Size(120, 21);
-            this.lblSuccessPercent.TabIndex = 44;
-            this.lblSuccessPercent.Text = "Success Percent";
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(230, 133);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(33, 17);
+            this.labelControl1.TabIndex = 45;
+            this.labelControl1.Text = "Beste";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(230, 179);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(39, 17);
+            this.labelControl2.TabIndex = 46;
+            this.labelControl2.Text = "Küçük";
+            // 
+            // svgImageBox1
+            // 
+            this.svgImageBox1.Location = new System.Drawing.Point(0, 403);
+            this.svgImageBox1.Name = "svgImageBox1";
+            this.svgImageBox1.Size = new System.Drawing.Size(131, 103);
+            this.svgImageBox1.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("svgImageBox1.SvgImage")));
+            this.svgImageBox1.TabIndex = 47;
+            this.svgImageBox1.Text = "svgImageBox1";
+            // 
+            // svgImageBox2
+            // 
+            this.svgImageBox2.Location = new System.Drawing.Point(742, 403);
+            this.svgImageBox2.Name = "svgImageBox2";
+            this.svgImageBox2.Size = new System.Drawing.Size(77, 103);
+            this.svgImageBox2.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("svgImageBox2.SvgImage")));
+            this.svgImageBox2.TabIndex = 48;
+            this.svgImageBox2.Text = "svgImageBox2";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Location = new System.Drawing.Point(363, 232);
+            this.labelControl3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(86, 17);
+            this.labelControl3.TabIndex = 49;
+            this.labelControl3.Text = "Subject Name";
+            // 
+            // progressBarControl1
+            // 
+            this.progressBarControl1.EditValue = 50;
+            this.progressBarControl1.Location = new System.Drawing.Point(482, 130);
+            this.progressBarControl1.Name = "progressBarControl1";
+            this.progressBarControl1.Size = new System.Drawing.Size(251, 32);
+            this.progressBarControl1.TabIndex = 53;
+            // 
+            // progressBarControl2
+            // 
+            this.progressBarControl2.EditValue = 10;
+            this.progressBarControl2.Location = new System.Drawing.Point(482, 232);
+            this.progressBarControl2.Name = "progressBarControl2";
+            this.progressBarControl2.Size = new System.Drawing.Size(251, 32);
+            this.progressBarControl2.TabIndex = 54;
             // 
             // Report
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.lblSuccessPercent);
+            this.Controls.Add(this.progressBarControl2);
+            this.Controls.Add(this.progressBarControl1);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.svgImageBox2);
+            this.Controls.Add(this.svgImageBox1);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.lblModuleName);
             this.Controls.Add(this.lblHeader);
             this.Controls.Add(this.lblLastName);
             this.Controls.Add(this.lblFirstName);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Report";
-            this.Size = new System.Drawing.Size(956, 623);
+            this.Size = new System.Drawing.Size(819, 506);
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.svgImageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl2.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,6 +191,12 @@ namespace training_exam_app
         private DevExpress.XtraEditors.LabelControl lblLastName;
         private DevExpress.XtraEditors.LabelControl lblHeader;
         private DevExpress.XtraEditors.LabelControl lblModuleName;
-        private DevExpress.XtraEditors.LabelControl lblSuccessPercent;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.SvgImageBox svgImageBox1;
+        private DevExpress.XtraEditors.SvgImageBox svgImageBox2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.ProgressBarControl progressBarControl1;
+        private DevExpress.XtraEditors.ProgressBarControl progressBarControl2;
     }
 }
